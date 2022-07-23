402fee1fb22a2c0730219f6e4d0e1fb515dbd650
dishita <dishita.bharadia@gmail.com>
2022-07-23 09:32:28 +0800

    Initial Commit

---
+++ Emoji Party/Emoji_PartyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji_PartyApp.swift
+//  Emoji Party
+//
+//  Created by T Krobot on 23/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Emoji_PartyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
